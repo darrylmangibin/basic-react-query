4938a215385ff25d9cc73e0b6bcf5c2e16d31142
Darryl Mangibin <darrylmangibin@gmail.com>
2021-04-29 15:05:50 +0800

    Add functionality for adding a todo

--- api/todos.ts
+++ api/todos.ts
@@ -1,4 +1,4 @@
-import axios from 'axios';
+import axios, { AxiosRequestConfig } from 'axios';
 
 import { Todo } from '../interface/Todo';
 
@@ -12,4 +12,23 @@ export const getTodo = async (id: string): Promise<Todo> => {
 	const { data } = await axios.get<Todo>(`http://localhost:5000/todos/${id}`);
 
 	return data;
+};
+
+export const addTodo = async (todo) => {
+	const config: AxiosRequestConfig = {
+		headers: {
+			'Content-Type': 'application/json',
+		},
+	};
+
+	try {
+		const { data } = await axios.post(
+			'http://localhost:5000/todos',
+			todo,
+			config
+		);
+		return data;
+	} catch (err) {
+		throw { message: 'Something went wrong' };
+	}
 };
--- pages/todos/create/index.tsx
+++ pages/todos/create/index.tsx
@@ -1,13 +1,38 @@
 import React from 'react';
 import { NextPage } from 'next';
+import { useMutation, useQueryClient } from 'react-query';
 
 import TodoForm from '../../../components/todos/todo-form';
+import { Todo, QueryKeyEnum } from '../../../interface/Todo';
+import { addTodo } from '../../../api/todos';
 
 const TodosCreatePage: NextPage = () => {
+	const queryClient = useQueryClient();
+
+	const mutation = useMutation(addTodo);
+
+	const onSubmit = (data: Partial<Todo>) => {
+		if (data.title.length <= 0) {
+			return window.alert('Please add a atitle');
+		}
+
+		mutation.mutate(data, {
+			onSuccess: (data) => {
+				if (queryClient.getQueryData(QueryKeyEnum.TODOS)) {
+					queryClient.setQueryData<Todo[]>(QueryKeyEnum.TODOS, (todos) => [
+						...todos,
+						data,
+					]);
+				}
+			},
+		});
+	};
+
 	return (
 		<div>
 			<h3>Create todo</h3>
-      <TodoForm />
+			<TodoForm onSubmit={onSubmit} />
+			{mutation.isLoading && <p>Loading...</p>}
 		</div>
 	);
 };
