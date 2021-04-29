6c9cb39f1249b297402c13a5851c29ab98c2f45d
Darryl Mangibin <darrylmangibin@gmail.com>
2021-04-29 11:43:07 +0800

    Add TodosPage

---
+++ pages/todos/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { NextPage } from 'next';
+
+const TodosPage: NextPage = () => {
+	return (
+		<div>
+			<h3>Todos</h3>
+			<ul></ul>
+		</div>
+	);
+};
+
+export default TodosPage;
