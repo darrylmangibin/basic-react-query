173d14e57c0ef2162ea774e7eec4d1bbf7a86a47
Darryl Mangibin <darrylmangibin@gmail.com>
2021-04-29 11:55:01 +0800

    Add form to create a todo

---
+++ components/todos/todo-form.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const TodoForm: React.FC = () => {
+	return (
+		<form>
+			<div>
+				<label htmlFor="title">Title: </label>
+				<input type="text" name="title" id="title" />
+			</div>
+			<div>
+				<label htmlFor="completed">Completed: </label>
+				<input type="checkbox" name="completed" id="completed" />
+			</div>
+			<div>
+				<button>Submit</button>
+			</div>
+		</form>
+	);
+};
+
+export default TodoForm;
--- pages/todos/create/index.tsx
+++ pages/todos/create/index.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import { NextPage } from 'next';
 
+import TodoForm from '../../../components/todos/todo-form';
+
 const TodosCreatePage: NextPage = () => {
 	return (
 		<div>
 			<h3>Create todo</h3>
+      <TodoForm />
 		</div>
 	);
 };
