9fac32f0f4a9885e67b238d4c70d60ed2bcdd35a
Darryl Mangibin <darrylmangibin@gmail.com>
2021-04-29 14:14:49 +0800

    Add form state to todo-form component

--- components/todos/todo-form.tsx
+++ components/todos/todo-form.tsx
@@ -1,15 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const TodoForm: React.FC = () => {
+	const [title, setTitle] = useState<string>('');
+	const [completed, setCompleted] = useState<boolean>(false);
+
 	return (
 		<form>
 			<div>
 				<label htmlFor="title">Title: </label>
-				<input type="text" name="title" id="title" />
+				<input
+					type="text"
+					name="title"
+					id="title"
+					value={title}
+					onChange={(e) => setTitle(e.target.value)}
+				/>
 			</div>
 			<div>
 				<label htmlFor="completed">Completed: </label>
-				<input type="checkbox" name="completed" id="completed" />
+				<input
+					type="checkbox"
+					name="completed"
+					id="completed"
+					checked={completed}
+					onChange={(e) => setCompleted(e.target.checked)}
+				/>
 			</div>
 			<div>
 				<button>Submit</button>
