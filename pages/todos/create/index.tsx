904580aa7add57e4ad7348e04941fcabbbac7175
Darryl Mangibin <darrylmangibin@gmail.com>
2021-04-29 11:51:57 +0800

    Add Todos Create page

---
+++ pages/todos/create/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { NextPage } from 'next';
+
+const TodosCreatePage: NextPage = () => {
+	return (
+		<div>
+			<h3>Create todo</h3>
+		</div>
+	);
+};
+
+export default TodosCreatePage;
